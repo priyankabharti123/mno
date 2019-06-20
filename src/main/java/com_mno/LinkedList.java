f27b2730b99e93701332f6d35aba769b073f53fb
priyankabharti123 <43872324+priyankabharti123@users.noreply.github.com>
2019-06-20 12:12:07 +0530

    Add files via upload

---
+++ src/main/java/com_mno/LinkedList.java
@@ -0,0 +1,20 @@
+package com_mno;
+
+public class LinkedList {
+
+	Node head;
+	 class Node
+	 { int data;
+	    Node next;
+		  Node(int d){
+			  data=d;
+			  next = null;
+		  }
+	 }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
